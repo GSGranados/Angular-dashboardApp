03159dbbc5206a133dbbcd9afad735ce55a6c45d
GSGranados <stevengranados0211@gmail.com>
2020-07-31 22:29:53 -0600

    Navegability applied and highcharts implemented. Service creation pending

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -6,6 +6,9 @@ import { RouterModule } from '@angular/router';
 import { PostsComponent } from '../../modules/posts/posts.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -18,6 +21,9 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     RouterModule,
     SharedModule,
     MatSidenavModule,
+    MatDividerModule,
+    FlexLayoutModule,
+    MatCardModule,
   ]
 })
 export class DefaultModule { }
